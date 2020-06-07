fb2517e0f0b030c8ba07999ed92265fff95f3f1d
unholyshadow <alberto9587@gmail.com>
2020-06-07 15:59:38 -0600

    update

---
+++ commit_files.sh
@@ -0,0 +1,8 @@
+cd  ~/parking_app_phonegap_01/SmartParking
+git init
+git config --global user.email "alberto9587@gmail.com"
+git config --global user.name "unholyshadow"
+git remote add origin https://github.com/unholyshadow/SmartParking.git
+git add .
+git commit -am "update"
+git push origin master
